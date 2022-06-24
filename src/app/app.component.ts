312d27ed298735c0a2434706769cf775418e5f2b
richieflicker <richardpeter07@gmail.com>
2022-06-24 21:36:45 +0530

    Initialize Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
-
+import { CrudHttpService } from './crud-http.service';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Test';
+  name = 'Test';
+
+  users:any = [];
+  constructor(private crudHttpService: CrudHttpService){}
+  
+  ngOnInit(): void {
+    this.listTodos();
+  }
+
+  listTodos(){
+    this.crudHttpService.list().subscribe((response)=>{
+      this.users = response;
+    },(error=>{
+
+    }));
+  }
+
+  createTodo(){
+    const input = document.getElementById("userName") as HTMLInputElement;
+    const value = input?.value;
+    console.log(value)
+    if (value != "") {
+      let todo = {
+        id: new Date().getTime(),
+        name:value
+      }
+      this.crudHttpService.create(todo).subscribe((response)=>{
+        this.listTodos();
+      },(error=>{
+  
+      }));
+    }else{
+      input.classList.add('error')
+    }
+    
+  }
+
+  editTodo(todo: any){
+    const input = document.getElementById(todo.id) as HTMLInputElement | null;
+    const value = input?.value;
+    let data = {
+      id: new Date().getTime(),
+      name:value 
+    }
+    this.crudHttpService.update(todo.id,data).subscribe((response)=>{
+      this.listTodos();
+    },(error=>{
+
+    }));
+  }
+
+  deleteTodo(id: any){
+    this.crudHttpService.delete(id).subscribe((response)=>{
+      this.listTodos();
+    },(error=>{
+    }));
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentNameComponent } from './component-name/component-name.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentNameComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
